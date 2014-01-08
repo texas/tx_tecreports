fdb2257fa4ad4a8da2c6657781355b48ba94fb38
Travis Swicegood <development@domain51.com>
2014-01-08 13:20:43 -0600

    Initial skelton

---
+++ README.rst
@@ -0,0 +1,23 @@
+tx_tecreports
+=============
+Python code for interacting with Texas Ethic Commision reports
+
+
+Installation & Configuration
+----------------------------
+You can install this using `pip`_ like this:
+
+::
+
+    pip install tx_tecreports
+
+Now you're ready to start using ``tx_tecreports``.
+
+
+Usage
+-----
+*TODO*
+
+
+.. _pip: http://www.pip-installer.org/en/latest/
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+from distutils.core import setup
+import os
+
+# Stolen from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('tx_tecreports'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[len('tx_tecreports/'):]
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
+
+setup(
+    name='tx_tecreports',
+    version='0.0.0',
+    description='Package for dealing with TEC reports',
+    author='Tribune Tech',
+    author_email='tech@texastribune.org',
+    url='https://github.com/texas/tx_tecreports/',
+    packages=packages,
+    package_data={'tx_tecreports': data_files},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
