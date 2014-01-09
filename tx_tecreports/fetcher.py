9a36f17758958916d6051634626b2a69f2c66b19
Travis Swicegood <development@domain51.com>
2014-01-09 14:07:28 -0600

    Remove unused/dead code

--- tx_tecreports/fetcher.py
+++ tx_tecreports/fetcher.py
@@ -1,12 +1,6 @@
-import datetime
-
 import requests
 
 from . import models
-
-
-def string_to_date(s):
-    return datetime.datetime.strptime(s, '%Y%m%d').date()
 
 
 BASE_URL_TEMPLATE = 'http://204.65.203.5/public/{report_id}noadd.csv'
