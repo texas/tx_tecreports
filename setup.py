f4f14a22f060f8b6c9c5c7aa8a30970f4dbcb194
Travis Swicegood <development@domain51.com>
2014-01-16 10:38:01 -0600

    Bump to next dev version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tx_tecreports'):
 
 setup(
     name='tx_tecreports',
-    version='0.6.0',
+    version='0.7alpha.0',
     description='Package for dealing with TEC reports',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
