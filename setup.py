56eff8099f084d2f20842c6025628ca1eeba40eb
Ryan Murphy <github@accounts.rdmurphy.org>
2014-02-05 11:05:57 -0600

    Bump to 0.8.2alpha

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tx_tecreports'):
 
 setup(
     name='tx_tecreports',
-    version='0.8.1',
+    version='0.8.2alpha',
     description='Package for dealing with TEC reports',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
@@ -45,4 +45,4 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
     ],
-)
+
