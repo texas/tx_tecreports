e46728cbce6e51c02513b00d7519172ecc96dc86
Travis Swicegood <development@domain51.com>
2014-02-24 16:26:56 -0600

    This is in production now (v1.0.0)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tx_tecreports'):
 
 setup(
     name='tx_tecreports',
-    version='0.8.2',
+    version='1.0.0',
     description='Package for dealing with TEC reports',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
